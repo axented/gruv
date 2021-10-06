36381772d930bfafd22a75951ea5abd6b4e00415
Gabriel Paredes <gabriel@axented.com>
2021-10-05 20:24:09 -0400

    Add copy and placeholder images

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,95 @@
 <template>
   <div>
-
+    <div class="bg-indigo-900 background-dancer">
+      <div class="container mx-auto py-12">
+        <img src="//placehold.it/300x100">
+        <div class="py-28">
+          <h1 class="text-gray-100 text-6xl font-bold leading-relaxed">Baila, ríe y diviértete<br>creando videos</h1>
+          <h4 class="text-gray-100 text-large font-bold leading-relaxed">Descarga grüv.it</h4>
+          <div class="flex flex-row pt-4">
+            <img class="pr-5" src="//placehold.it/150x50">
+            <img class="pr-5" src="//placehold.it/150x50">
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="container mx-auto py-12">
+      <div class="grid grid-cols-3">
+        <div class="col-span-1">
+          <img src="//placehold.it/900">
+        </div>
+        <div class="col-span-2">
+          <h2 class="text-4xl font-bold">Juega Remotamente</h2>
+          <p>Disfruta de la compañía de tus amigos y familiares de manera fácil y remota.</p>
+        </div>
+      </div>
+    </div>
+    <div class="bg-indigo-100">
+      <div class="container mx-auto py-12">
+        <h2 class="text-4xl font-bold leading-normal pb-24">Personaliza tus grüvs <br> como quieras</h2>
+        <div class="flex flex-row">
+          <div class="space-y-10 flex-grow">
+            <div class="flex">
+              <img src="//placehold.it/400" alt="" class="flex-none w-18 h-18 rounded-full object-cover bg-gray-100 mr-8" width="50" height="50" />
+              <div class="min-w-0 relative flex-auto">
+                <h2 class="text-lg font-semibold text-black mb-0.5">Lobby</h2>
+                <p>Crea un lobby e invita a tus amigos</p>
+              </div>
+            </div>
+            <div class="flex">
+              <img src="//placehold.it/400" alt="" class="flex-none w-18 h-18 rounded-full object-cover bg-gray-100 mr-8" width="50" height="50" />
+              <div class="min-w-0 relative flex-auto">
+                <h2 class="text-lg font-semibold text-black mb-0.5">Crea</h2>
+                <p>Grábate siguiendo un video ya existente o haciendo algo único.</p>
+              </div>
+            </div>
+            <div class="flex">
+              <img src="//placehold.it/400" alt="" class="flex-none w-18 h-18 rounded-full object-cover bg-gray-100 mr-8" width="50" height="50" />
+              <div class="min-w-0 relative flex-auto">
+                <h2 class="text-lg font-semibold text-black mb-0.5">Espera</h2>
+                <p>A que tus amigos graben sus videos.</p>
+              </div>
+            </div>
+            <div class="flex">
+              <img src="//placehold.it/400" alt="" class="flex-none w-18 h-18 rounded-full object-cover bg-gray-100 mr-8" width="50" height="50" />
+              <div class="min-w-0 relative flex-auto">
+                <h2 class="text-lg font-semibold text-black mb-0.5">grüv</h2>
+                <p>Disfruta de ver el video que crearon juntos.</p>
+              </div>
+            </div>
+          </div>
+          <div class="flex-grow">
+            <img src="//placehold.it/200x400">
+          </div>
+        </div>
+      </div>
+      <div class="bg-gray-900">
+        <div class="container mx-auto py-10">
+          <div class="flex flex-row">
+            <div class="flex-grow">
+              <img src="//placehold.it/40">
+              <div class="flex flex-row pt-4 space-x-4">
+                <img src="//placehold.it/40">
+                <img src="//placehold.it/40">
+                <img src="//placehold.it/40">
+              </div>
+              <div class="flex flex-row space-x-4">
+                <p class="text-gray-100">Políticas de privacidad</p>
+                <p class="text-gray-100">Términos y Condiciones</p>
+              </div>
+            </div>
+            <div class="flex-none space-y-5">
+              <h4 class="text-gray-100 text-large font-bold leading-relaxed">Descarga la app</h4>
+              <img src="//placehold.it/150x50">
+              <img src="//placehold.it/150x50">
+            </div>
+          </div>
+          <div>
+            <p class="text-gray-300 text-xs">© 2021  Todos los derechos reservados</p>
+          </div>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
