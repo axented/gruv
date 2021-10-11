73f5c61bbca95fa5c28c8f0649f37d3df5b19763
Rafael Serna <rafaelserna190@gmail.com>
2021-10-11 10:36:21 -0500

    Added responsiveness (#3)
    Co-authored-by: Rafael Serna <rafael.serna@axented.com>

--- src/App.vue
+++ src/App.vue
@@ -1,36 +1,37 @@
 <template>
   <div>
     <div class="bg-indigo-900 background-dancer">
-      <div class="container mx-auto py-12 relative">
-        <img src="./assets/gruv-logo.png">
-        <div class="py-28">
+      <div class="container py-12 relative">
+        <img src="./assets/gruv-logo.png" class="mx-auto lg:mx-0">
+        <div class="text-center lg:text-left py-16 lg:py-28">
           <h1 class="text-gray-100 text-6xl font-bold leading-relaxed">Baila, ríe y diviértete<br>creando videos</h1>
           <h4 class="text-gray-100 text-large font-bold leading-relaxed">Descarga grüv.it</h4>
-          <div class="flex flex-row pt-4">
-            <img class="pr-5 h-10" src="./assets/appstore-badge.png">
-            <img class="pr-5 h-10" src="./assets/google-play-badge.png">
+          <div class="flex flex-row justify-center lg:justify-start pt-4">
+            <img class="pr-2 lg:pr-5 h-10" src="./assets/appstore-badge.png">
+            <img class="lg:pr-5 h-10" src="./assets/google-play-badge.png">
           </div>
         </div>
-        <img class="absolute bottom-0 right-0 w-1/2" src="./assets/girl.png">
+        <img class="hidden lg:block absolute bottom-0 right-0 w-1/2" src="./assets/girl.png">
       </div>
     </div>
-    <div class="container mx-auto">
-      <div class="grid grid-cols-5">
+    <div class="container">
+      <div class="flex flex-col lg:flex-row">
         <div class="col-span-2 flex flex-row items-stretch">
-          <div class="flex align-start"><img src="./assets/phone_mockup_1.png"></div>
-          <div class="flex align-end"><img src="./assets/phone_mockup_2.png"></div>
+          <div class="flex w-full lg:w-auto align-start"><img src="./assets/phone_mockup_1.png" class="w-1/2 lg:w-full"></div>
+          <div class="hidden lg:flex lg:w-auto align-end"><img src="./assets/phone_mockup_2.png" class="w-1/2 lg:w-full"></div>
         </div>
-        <div class="col-span-3 py-40">
+        <div class="col-span-3 text-center py-12 lg:text-left lg:py-40">
           <h2 class="text-4xl font-bold">Juega Remotamente</h2>
           <p>Disfruta de la compañía de tus amigos y familiares de manera fácil y remota.</p>
         </div>
+        <div class="flex w-full lg:hidden justify-end pt-4 align-end"><img src="./assets/phone_mockup_2.png" class="w-1/2"></div>
       </div>
     </div>
     <div class="bg-offwhite">
-      <div class="container mx-auto py-12">
-        <h2 class="text-4xl font-bold leading-normal pb-24">Personaliza tus grüvs <br> como quieras</h2>
-        <div class="grid grid-cols-2 gap-5">
-          <div class="space-y-10 col-span-1">
+      <div class="container py-12">
+        <h2 class="text-4xl text-center lg:text-left font-bold leading-normal pb-12 lg:pb-24">Personaliza tus grüvs <br> como quieras</h2>
+        <div class="flex flex-col lg:flex-row">
+          <div class="flex flex-col lg:w-1/2">
             <div class="flex">
               <LightningBoltIcon class="flex-none w-16 h-16 rounded-full bg-white mr-8 p-5 text-primary" width="50" height="50"/>
               <div class="min-w-0 relative flex-auto">
@@ -38,28 +39,28 @@
                 <p>Crea un lobby e invita a tus amigos y familiares. Chatea con ellos mientras esperan a que todos se unan al juego.</p>
               </div>
             </div>
-            <div class="flex">
+            <div class="flex mt-10">
               <CameraIcon class="flex-none w-16 h-16 rounded-full bg-white mr-8 p-5 text-primary" width="50" height="50"/>
               <div class="min-w-0 relative flex-auto">
                 <h2 class="text-lg font-semibold text-black mb-0.5">Crea</h2>
                 <p>El primer jugador en la cadena actúa frente a su cámara haciendo algunos pasos de baile, movimientos corporales, canto, declamación o incluso contar un chiste.</p>
               </div>
             </div>
-            <div class="flex">
+            <div class="flex mt-10">
               <img src="./assets/logo-small.png" alt="gruv logo" class="flex-none w-16 h-16 rounded-full object-cover bg-white mr-8 p-5" />
               <div class="min-w-0 relative flex-auto">
                 <h2 class="text-lg font-semibold text-black mb-0.5">Continúa</h2>
                 <p>Cada jugador en la línea visualiza una sola vez el video creado por el participante anterior y se graba tratando de imitar lo que vio (es más difícil de lo que parece).</p>
               </div>
             </div>
-            <div class="flex">
+            <div class="flex mt-10">
               <ClockIcon class="flex-none w-16 h-16 rounded-full bg-white mr-8 p-5 text-primary" width="50" height="50"/>
               <div class="min-w-0 relative flex-auto">
                 <h2 class="text-lg font-semibold text-black mb-0.5">Espera</h2>
                 <p>Una vez que todos han grabado su video, la plataforma los presenta en secuencia. Esto es un grüv.</p>
               </div>
             </div>
-            <div class="flex">
+            <div class="flex mt-10">
               <img src="./assets/logo-small.png" alt="gruv logo" class="flex-none w-16 h-16 rounded-full object-cover bg-white mr-8 p-5" />
               <div class="min-w-0 relative flex-auto">
                 <h2 class="text-lg font-semibold text-black mb-0.5">grüv</h2>
@@ -67,29 +68,35 @@
               </div>
             </div>
           </div>
-          <div class="col-span-1">
+          <div class="flex w-full lg:w-1/2 mt-12 lg:mt-0">
             <img class="d-block mx-auto" src="./assets/phone_mockup_3.png">
           </div>
         </div>
       </div>
       <div class="bg-almostblack">
-        <div class="container mx-auto py-10">
-          <div class="flex flex-row">
-            <div class="flex-grow">
-              <img src="./assets/logo-small.png">
-              <div class="flex flex-row space-x-4 pt-4">
-                <p class="text-gray-100">Políticas de privacidad</p>
-                <p class="text-gray-100">Términos y Condiciones</p>
-                <p class="text-gray-100">Ayuda</p>
+        <div class="container py-10">
+          <div class="flex flex-col lg:flex-row">
+            <div class="flex-grow flex flex-col justify-center lg:justify-start">
+              <div class="flex justify-center lg:justify-start">
+                <img src="./assets/logo-small.png" />
+              </div>
+              <div class="flex flex-col lg:flex-row text-center lg:text-left pt-4">
+                <p class="text-gray-100 pt-2 lg:pt-0">Políticas de privacidad</p>
+                <p class="text-gray-100 pt-6 lg:pt-0 lg:pl-4">Términos y Condiciones</p>
+                <p class="text-gray-100 pt-6 lg:pt-0 lg:pl-4">Ayuda</p>
               </div>
             </div>
-            <div class="flex-none space-y-5">
-              <h4 class="text-gray-100 text-large font-bold leading-relaxed">Descarga la app</h4>
-              <img src="./assets/appstore-badge.png">
-              <img src="./assets/google-play-badge.png">
+            <div class="flex flex-col pt-10 lg:pt-0">
+              <h4 class="text-gray-100 text-center lg:text-left text-large font-bold leading-relaxed">Descarga la app</h4>
+              <div class="flex justify-center lg:justify-start pt-6">
+                <img src="./assets/appstore-badge.png">
+              </div>
+              <div class="flex justify-center lg:justify-start pt-6">
+                <img src="./assets/google-play-badge.png">
+              </div>  
             </div>
           </div>
-          <div>
+          <div class="pt-12 lg:pt-0">
             <p class="text-gray-300 text-xs">Correo de soporte: gruv.itapp@gmail.com</p>
             <p class="text-gray-300 text-xs">© 2021  Todos los derechos reservados</p>
           </div>
