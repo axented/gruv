9e7f9ab285e63f36bcafb32764e215254a746ccc
Gabriel Paredes <gabriel@axented.com>
2021-11-12 19:36:44 -0500

    (WIP) Add i18n package and implementation

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -4,8 +4,8 @@
       <div class="container py-12 relative">
         <img src=".././assets/gruv-logo.png" class="mx-auto lg:mx-0">
         <div class="text-center lg:text-left py-16 lg:py-28">
-          <h1 class="text-gray-100 text-6xl font-bold leading-relaxed">Modernizamos el <br>clásico "teléfono <br>descompuesto"</h1>
-          <h4 class="text-gray-100 text-large font-bold leading-relaxed">Descarga grüv.it</h4>
+          <h1 class="text-gray-100 text-6xl font-bold leading-relaxed"> {{ $t('hero.title') }} </h1>
+          <h4 class="text-gray-100 text-large font-bold leading-relaxed"> {{ $t('hero.subtitle') }} </h4>
           <div class="flex flex-row justify-center lg:justify-start pt-4">
             <a href="https://apps.apple.com/us/app/gr%C3%BCv-it/id1585058785" target="_blank">
               <img class="pr-2 lg:pr-5 h-10" src=".././assets/appstore-badge.png">
@@ -25,50 +25,50 @@
           <div class="hidden lg:flex lg:w-auto align-end"><img src=".././assets/phone_mockup_2.png" class="w-1/2 lg:w-full"></div>
         </div>
         <div class="col-span-3 text-center py-12 lg:text-left lg:py-40">
-          <h2 class="text-4xl font-bold">Juega Remotamente</h2>
-          <p>Nuestro propósito es conectar a las personas e inspirarlas <br> para expresarse de una forma alegrey creativa.</p>
+          <h2 class="text-4xl font-bold">{{ $t('banner.title') }}</h2>
+          <p>{{ $t('banner.subtitle') }}</p>
         </div>
         <div class="flex w-full lg:hidden justify-end pt-4 align-end"><img src=".././assets/phone_mockup_2.png" class="w-1/2"></div>
       </div>
     </div>
     <div class="bg-offwhite">
       <div class="container py-12">
-        <h2 class="text-4xl text-center lg:text-left font-bold leading-normal pb-12 lg:pb-24">Personaliza tus grüvs <br> como quieras</h2>
+        <h2 class="text-4xl text-center lg:text-left font-bold leading-normal pb-12 lg:pb-24">{{ $t('content.title') }}</h2>
         <div class="flex flex-col lg:flex-row">
           <div class="flex flex-col lg:w-1/2">
             <div class="flex">
               <LightningBoltIcon class="flex-none w-16 h-16 rounded-full bg-white mr-8 p-5 text-primary" width="50" height="50"/>
               <div class="min-w-0 relative flex-auto">
-                <h2 class="text-lg font-semibold text-black mb-0.5">Lobby</h2>
-                <p>Crea un lobby e invita a tus amigos y familiares. Chatea con ellos mientras esperan a que todos se unan al juego.</p>
+                <h2 class="text-lg font-semibold text-black mb-0.5">{{ $t('content.list[0].title') }}</h2>
+                <p>{{ $t('content.list[0].text') }}</p>
               </div>
             </div>
             <div class="flex mt-10">
               <CameraIcon class="flex-none w-16 h-16 rounded-full bg-white mr-8 p-5 text-primary" width="50" height="50"/>
               <div class="min-w-0 relative flex-auto">
-                <h2 class="text-lg font-semibold text-black mb-0.5">Crea</h2>
-                <p>El primer jugador en la cadena actúa frente a su cámara haciendo algunos pasos de baile, movimientos corporales, canto, declamación o incluso contar un chiste.</p>
+                <h2 class="text-lg font-semibold text-black mb-0.5">{{ $t('content.list[1].title') }}</h2>
+                <p>{{ $t('content.list[1].text') }}</p>
               </div>
             </div>
             <div class="flex mt-10">
               <img src=".././assets/logo-small.png" alt="gruv logo" class="flex-none w-16 h-16 rounded-full object-cover bg-white mr-8 p-5" />
               <div class="min-w-0 relative flex-auto">
-                <h2 class="text-lg font-semibold text-black mb-0.5">Continúa</h2>
-                <p>Cada jugador en la línea visualiza una sola vez el video creado por el participante anterior y se graba tratando de imitar lo que vio (es más difícil de lo que parece).</p>
+                <h2 class="text-lg font-semibold text-black mb-0.5">{{ $t('content.list[2].title') }}</h2>
+                <p>{{ $t('content.list[2].text') }}</p>
               </div>
             </div>
             <div class="flex mt-10">
               <ClockIcon class="flex-none w-16 h-16 rounded-full bg-white mr-8 p-5 text-primary" width="50" height="50"/>
               <div class="min-w-0 relative flex-auto">
-                <h2 class="text-lg font-semibold text-black mb-0.5">Espera</h2>
-                <p>Una vez que todos han grabado su video, la plataforma los presenta en secuencia. Esto es un grüv.</p>
+                <h2 class="text-lg font-semibold text-black mb-0.5">{{ $t('content.list[3].title') }}</h2>
+                <p>{{ $t('content.list[3].text') }}</p>
               </div>
             </div>
             <div class="flex mt-10">
               <img src=".././assets/logo-small.png" alt="gruv logo" class="flex-none w-16 h-16 rounded-full object-cover bg-white mr-8 p-5" />
               <div class="min-w-0 relative flex-auto">
-                <h2 class="text-lg font-semibold text-black mb-0.5">grüv</h2>
-                <p>El objetivo del juego es imitar el video tal como aparece en pantalla. Notarás cómo cada participante interpreta el reto a su estilo y el efecto acumulado de las diferencias.</p>
+                <h2 class="text-lg font-semibold text-black mb-0.5">{{ $t('content.list[4].title') }}</h2>
+                <p>{{ $t('content.list[4].text') }}</p>
               </div>
             </div>
           </div>
@@ -85,14 +85,14 @@
                 <img src=".././assets/logo-small.png" />
               </div>
               <div class="flex flex-col lg:flex-row text-center lg:text-left pt-4">
-                <p class="text-gray-100 pt-6 lg:pt-0"><router-link to="/EULA">EULA</router-link></p>
-                <p class="text-gray-100 pt-6 lg:pt-0 lg:pl-4"><router-link to="/terms">Términos y Condiciones</router-link></p>
-                <p class="text-gray-100 pt-2 lg:pt-0 lg:pl-4"><router-link to="/privacy">Políticas de privacidad</router-link></p>
-                <p class="text-gray-100 pt-6 lg:pt-0 lg:pl-4"><router-link to="/rules">Nuestras Reglas</router-link></p>
+                <p class="text-gray-100 pt-6 lg:pt-0"><router-link to="/EULA">{{ $t('footer.links.eula') }}</router-link></p>
+                <p class="text-gray-100 pt-6 lg:pt-0 lg:pl-4"><router-link to="/terms">{{ $t('footer.links.tandc') }}</router-link></p>
+                <p class="text-gray-100 pt-2 lg:pt-0 lg:pl-4"><router-link to="/privacy">{{ $t('footer.links.privacy') }}</router-link></p>
+                <p class="text-gray-100 pt-6 lg:pt-0 lg:pl-4"><router-link to="/rules">{{ $t('footer.links.rules') }}</router-link></p>
               </div>
             </div>
             <div class="flex flex-col pt-10 lg:pt-0">
-              <h4 class="text-gray-100 text-center lg:text-left text-large font-bold leading-relaxed">Descarga la app</h4>
+              <h4 class="text-gray-100 text-center lg:text-left text-large font-bold leading-relaxed">{{ $t('footer.contact') }}</h4>
               <div class="flex justify-center lg:justify-start pt-6">
                 <img src=".././assets/appstore-badge.png">
               </div>
@@ -102,8 +102,8 @@
             </div>
           </div>
           <div class="pt-12 lg:pt-0">
-            <p class="text-gray-300 text-xs">Correo de soporte: gruv.itapp@gmail.com</p>
-            <p class="text-gray-300 text-xs">© 2021  Todos los derechos reservados</p>
+            <p class="text-gray-300 text-xs">{{ $t('footer.contact') }}gruv.itapp@gmail.com</p>
+            <p class="text-gray-300 text-xs">{{ $t('footer.rights') }}</p>
           </div>
         </div>
       </div>
